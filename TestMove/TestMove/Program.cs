8e1305b94b886397309f8d966ddeed79d32ca8de
apetrovskiy <apetrovskiy@hotmail.com>
2015-05-07 20:15:31 +0300

    .

--- TestMove/TestMove/Program.cs
+++ TestMove/TestMove/Program.cs
@@ -5,10 +5,18 @@ using System.Text;
 
 namespace TestMove
 {
+    using ObjectModel;
+    using ObjectModel.Folder01;
+    using ObjectModel.Folder02;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var cl001 = new Class001();
+            var cl002 = new Class002();
+            var cl003 = new Class003();
+            var cl004 = new Class004();
         }
     }
 }
