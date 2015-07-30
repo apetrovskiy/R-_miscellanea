5de44d5dbd744428bfac350646c447959d13ba26
apetrovskiy <apetrovskiy@hotmail.com>
2015-07-30 16:26:56 +0300

    .

---
+++ RefactoringOfSettingsFile/MyLib/MyConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLib
+{
+    public class MyConstants
+    {
+        public static string Setting01 { get { return My.Default.Setting01; } }
+        public static string Setting02 { get { return My.Default.Setting02; } }
+        public static string Setting03 { get { return My.Default.Setting03; } }
+    }
+}
---
+++ RefactoringOfSettingsFile/RefactoringOfSettingsFile/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RefactoringOfSettingsFile
+{
+    using System;
+    using MyLib;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyConstants.Setting01);
+            Console.WriteLine(MyConstants.Setting02);
+            Console.WriteLine(MyConstants.Setting03);
+
+            Console.Read();
+        }
+    }
+}
