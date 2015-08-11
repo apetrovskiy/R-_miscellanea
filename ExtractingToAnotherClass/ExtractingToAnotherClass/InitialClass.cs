8846506631eaa2814eb3abac90a31deb57984380
apetrovskiy <apetrovskiy@hotmail.com>
2015-08-11 16:06:47 +0300

    Extracting to another class (to a new class) - initial state

---
+++ ExtractingToAnotherClass/ExtractingToAnotherClass/InitialClass.cs
@@ -0,0 +1,50 @@
+﻿namespace ExtractingToAnotherClass
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class InitialClass
+    {
+        public void SomeMethod01ToStayHere(string param1)
+        {
+            
+        }
+
+        public void SomeMethod02ToStayHere(int param1)
+        {
+            
+        }
+
+        public void SomeMethod03ToBeExtracted(long param1)
+        {
+            
+        }
+
+        public void SomeMethod04ToBeExtracted(string[] param1)
+        {
+            
+        }
+
+        public void SomeMethod05ToStayHere(string param1)
+        {
+
+        }
+
+        public void SomeMethod06ToStayHere(int param1)
+        {
+
+        }
+
+        public void SomeMethod07ToBeExtracted(long param1)
+        {
+
+        }
+
+        public void SomeMethod08ToBeExtracted(string[] param1)
+        {
+
+        }
+    }
+}
