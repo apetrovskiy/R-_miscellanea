cf9c47cdd81cb9d1699edb0f5056f3da9979968c
apetrovskiy <apetrovskiy@hotmail.com>
2015-09-09 08:37:37 +0300

    RenamingOfOverrides

---
+++ RenamingOfOverrides/RenamingOfOverrides/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RenamingOfOverrides
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new SomeType().SomeCaseOfUsage());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RenamingOfOverrides/RenamingOfOverrides/SomeType.cs
@@ -0,0 +1,20 @@
+﻿namespace RenamingOfOverrides
+{
+    public class SomeType
+    {
+        public string SomeCaseOfUsage()
+        {
+            return getMyData(1);
+        }
+
+        string getMyData(int param1)
+        {
+            return getMyData(param1, "param2");
+        }
+
+        string getMyData(int param1, params string[] param2)
+        {
+            return param1.ToString() + param2;
+        } 
+    }
+}
