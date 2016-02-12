958f9c5c1fbcacab8b31a2d1a40eda286a274e71
apetrovskiy <apetrovskiy@hotmail.com>
2016-02-12 22:04:53 +0300

    NotAllEnumItemsInspection

---
+++ NotAllEnumItemsInspection/NotAllEnumItemsInspection/SomeClass.cs
@@ -0,0 +1,45 @@
+﻿namespace NotAllEnumItemsInspection
+{
+    using System;
+
+    public class SomeClass
+    {
+        public void SomeMethod01(int myParam)
+        {
+            switch (myParam)
+            {
+                case (int)SomeEnum.One:
+                    break;
+                case (int)SomeEnum.Two:
+                    break;
+                case (int)SomeEnum.Four:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("myParam", myParam, null);
+            }
+        }
+
+        public void SomeMethod02(SomeEnum myParam)
+        {
+            switch (myParam)
+            {
+                case SomeEnum.One:
+                    break;
+                case SomeEnum.Two:
+                    break;
+                case SomeEnum.Four:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("myParam", myParam, null);
+            }
+        }
+    }
+
+    public enum SomeEnum
+    {
+        One,
+        Two,
+        Three,
+        Four
+    }
+}
